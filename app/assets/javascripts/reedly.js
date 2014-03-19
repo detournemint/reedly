b733fb126d698f0e8f45c602ea27eb5069cb0ea0
Gregg Horton <greggahorton@gmail.com>
2014-03-19 14:17:42 -0700

    breaking out sidebar and moving around submit box

--- app/assets/javascripts/reedly.js
+++ app/assets/javascripts/reedly.js
@@ -4,17 +4,25 @@ window.Reedly = {
   Views: {},
   Routers: {},
   initialize: function() {
-    var $rootEl = $('#content')
+    var $rootEl = $('#content');
+    var $sidebar = $('#sidebar');
     var feeds = new Reedly.Collections.Feeds();
+
+    var feedsIndexView = new Reedly.Views.FeedIndexView({
+      collection: feeds
+    });
+    $sidebar.html(feedsIndexView.render().$el);
+
     feeds.fetch({
       success: function(){
-        new Reedly.Routers.FeedRouter(feeds, $rootEl);
+        new Reedly.Routers.FeedRouter(feeds, $rootEl, sidebar);
         Backbone.history.start();
       },
       error: function(){
         console.log("fetch failed");
       }
     });
+
   }
 };
 
