f0117edce6a69f96a56c17b10f7928ee6777a6be
Gregg Horton <greggahorton@gmail.com>
2014-03-30 19:25:10 -0700

    updating seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.find_by_email('guest@guest.com').destroy
+
 user = User.create(email: 'guest@guest.com', password: 'guestguest')
 
 # make feeds
--- lib/tasks/schedule.rake
+++
@@ -1,8 +0,0 @@
-desc "This task is called by the Heroku scheduler add-on"
-task :refresh_db => :environment do
-  puts "Dropping DB"
-  %x(heroku pg:reset postgres)
-  %x(heroku run rake db:migrate)
-  %x(heroku run rake db:seed)
-  puts "done."
-end
