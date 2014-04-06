e5f9e38a117685978e0dbfb4f767f94d16dbb598
Gregg Horton <greggahorton@gmail.com>
2014-04-06 16:13:19 -0700

    title validation for feed category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,6 +6,9 @@ describe FeedCategory do
     expect(category.title).to eq("Testing")
   end
 
-  
+  it "validates title presence" do
+    category = FeedCategory.create!({title: ""})
+    expect(category).to have_at_least(1).error_on(:title)
+  end
 
 end
